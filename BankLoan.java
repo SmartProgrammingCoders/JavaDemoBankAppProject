30cfc9f33e993b49448504a34c04d713fe813eee
Smart Programming <coders.smartprogramming@gmail.com>
2025-08-08 20:08:39 +0530

    Amit Mumbai - BankLoan Main Method
    Just created main method as demo

--- BankLoan.java
+++ BankLoan.java
@@ -1,4 +1,8 @@
 public class BankLoan
 {
-
+    //this code is provided by another developer
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Bank Loan Application");
+    }
 }
